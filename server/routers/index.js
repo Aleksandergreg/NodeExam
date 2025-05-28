b3b4207d0b72d868b000816afcddf817e4806700
Aleksandergreg <alek3691@stud.kea.dk>
2025-05-28 07:59:31 +0200

    Setup the router functionality for the cycling part

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRouter from './authRouter.js';
 import protectedRouter from './protectedRouter.js';
+import racesRouter from './racesRouter.js';
 
 const router = Router();
 router.use(authRouter);
 router.use(protectedRouter);
+router.use(racesRouter);
 export default router;
