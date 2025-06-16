6d016f93aa1baa6ec8516dda3de22ef01eeee9e3
Aleksandergreg <alek3691@stud.kea.dk>
2025-06-16 19:20:18 +0200

    Made the currentYear in my footer dynamic

--- client/src/components/Footer/Footer.svelte
+++ client/src/components/Footer/Footer.svelte
@@ -1,9 +1,11 @@
 <script>
     import "../../styles/Footer.css";
+
+    const currentYear = new Date().getFullYear();
   </script>
   
   <footer class="app-footer">
     <div class="container">
-      <p>CyclingHub © 2025 - All Rights Reserved.</p>
+      <p>CyclingHub © {currentYear} - All Rights Reserved.</p>
     </div>
   </footer>
